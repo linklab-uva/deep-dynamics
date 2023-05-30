647b93cd398dc8853d1ed5644e76a030d30b8b72
john-chrosniak <chrosniakj@gmail.com>
2023-05-30 08:44:12 -0400

    added base class for deep dynamics model

--- model/models.py
+++ model/models.py
@@ -4,6 +4,7 @@ from build_network import build_network, string_to_torch
 import yaml
 import pickle
 import numpy as np
+from abc import abstractmethod
 
 
 if torch.cuda.is_available():
@@ -26,8 +27,7 @@ class DeepDynamicsDataset(torch.utils.data.Dataset):
         split_id = int(len(self)* 0.8)
         return torch.utils.data.random_split(self, [split_id, (len(self) - split_id)])
 
-
-class DeepDynamicsModel(nn.Module):
+class ModelBase(nn.Module):
     def __init__(self, param_dict):
         super().__init__()
         self.param_dict = param_dict
@@ -49,6 +49,9 @@ class DeepDynamicsModel(nn.Module):
         self.sys_params = list([*(list(p.keys())[0] for p in self.param_dict["PARAMETERS"])])
         self.vehicle_specs = self.param_dict["VEHICLE_SPECS"]
 
+    @abstractmethod
+    def differential_equation(self, x, output):
+        pass
 
     def forward(self, x, h0=None):
         for i in range(len(self.feed_forward)):
@@ -64,28 +67,7 @@ class DeepDynamicsModel(nn.Module):
                     ff = self.feed_forward[i](ff)
         o = self.differential_equation(x, ff)
         return o, h0, ff
-        
-
-    def differential_equation(self, x, output):
-        sys_param_dict, _ = self.unpack_sys_params(output)
-        state_action_dict = self.unpack_state_actions(x)
-        steering = state_action_dict["STEERING_FB"] + state_action_dict["STEERING_CMD"]
-        throttle = state_action_dict["THROTTLE_FB"] + state_action_dict["THROTTLE_CMD"]
-        alphaf = steering - torch.atan2(self.vehicle_specs["lf"]*state_action_dict["YAW_RATE"] + state_action_dict["VY"], torch.abs(state_action_dict["VX"]))
-        alphar = torch.atan2((self.vehicle_specs["lr"]*state_action_dict["YAW_RATE"] - state_action_dict["VY"]), torch.abs(state_action_dict["VX"]))
-        Frx = (sys_param_dict["Cm1"]-sys_param_dict["Cm2"]*state_action_dict["VX"])*throttle - sys_param_dict["Cr0"] - sys_param_dict["Cr2"]*(state_action_dict["VX"]**2)
-        Ffy = sys_param_dict["Df"] * torch.sin(sys_param_dict["Cf"] * torch.atan(sys_param_dict["Bf"] * alphaf))
-        Fry = sys_param_dict["Dr"] * torch.sin(sys_param_dict["Cr"] * torch.atan(sys_param_dict["Br"] * alphar))
-        dxdt = torch.zeros(len(x), len(self.state)).to(device)
-        dxdt[:,0] = 1/self.vehicle_specs["mass"] * (Frx - Ffy*torch.sin(steering)) + state_action_dict["VY"]*state_action_dict["YAW_RATE"]
-        dxdt[:,1] = 1/self.vehicle_specs["mass"] * (Fry + Ffy*torch.cos(steering)) - state_action_dict["VX"]*state_action_dict["YAW_RATE"]
-        dxdt[:,2] = 1/sys_param_dict["Izz"] * (Ffy*self.vehicle_specs["lf"]*torch.cos(steering) - Fry*self.vehicle_specs["lr"])
-        dxdt[:,3] = state_action_dict["THROTTLE_CMD"]
-        dxdt[:,4] = state_action_dict["STEERING_CMD"]
-        dxdt[:,:3] *= state_action_dict["delta_t"][:, None]
-        return x[:,-1,:len(self.state)] + dxdt
-
-
+    
     def test_sys_params(self, x):
         _, sys_param_dict = self.unpack_sys_params(torch.zeros((1, len(self.sys_params))))
         state_action_dict = self.unpack_state_actions(x)
@@ -131,48 +113,35 @@ class DeepDynamicsModel(nn.Module):
         weight = next(self.parameters()).data
         hidden = weight.new(self.rnn_n_layers, batch_size, self.rnn_hiden_dim).zero_().to(device)
         return hidden
+
     
-
-
-
-class DeepPacejkaModel(nn.Module):
+class DeepDynamicsModel(ModelBase):
     def __init__(self, param_dict):
-        super().__init__()
-        self.param_dict = param_dict
-        layers = build_network(self.param_dict)
-        self.batch_size = self.param_dict["MODEL"]["OPTIMIZATION"]["BATCH_SIZE"]
-        if self.param_dict["MODEL"]["LAYERS"][0].get("LAYERS"):
-            self.is_rnn = True
-            self.rnn_n_layers = self.param_dict["MODEL"]["LAYERS"][0].get("LAYERS")
-            self.rnn_hiden_dim = self.param_dict["MODEL"]["HORIZON"]
-            layers.insert(1, nn.Flatten())
-        else:
-            self.is_rnn = False
-        self.feed_forward = nn.ModuleList(layers)
-        self.loss_function = string_to_torch[self.param_dict["MODEL"]["OPTIMIZATION"]["LOSS"]]()
-        self.optimizer = string_to_torch[self.param_dict["MODEL"]["OPTIMIZATION"]["OPTIMIZER"]](self.parameters(), lr=self.param_dict["MODEL"]["OPTIMIZATION"]["LR"])
-        self.epochs = self.param_dict["MODEL"]["OPTIMIZATION"]["NUM_EPOCHS"]
-        self.state = list(self.param_dict["STATE"])
-        self.actions = list(self.param_dict["ACTIONS"])
-        self.sys_params = list([*(list(p.keys())[0] for p in self.param_dict["PARAMETERS"])])
-        self.vehicle_specs = self.param_dict["VEHICLE_SPECS"]
+        super().__init__(param_dict)
+
+    def differential_equation(self, x, output):
+        sys_param_dict, _ = self.unpack_sys_params(output)
+        state_action_dict = self.unpack_state_actions(x)
+        steering = state_action_dict["STEERING_FB"] + state_action_dict["STEERING_CMD"]
+        throttle = state_action_dict["THROTTLE_FB"] + state_action_dict["THROTTLE_CMD"]
+        alphaf = steering - torch.atan2(self.vehicle_specs["lf"]*state_action_dict["YAW_RATE"] + state_action_dict["VY"], torch.abs(state_action_dict["VX"]))
+        alphar = torch.atan2((self.vehicle_specs["lr"]*state_action_dict["YAW_RATE"] - state_action_dict["VY"]), torch.abs(state_action_dict["VX"]))
+        Frx = (sys_param_dict["Cm1"]-sys_param_dict["Cm2"]*state_action_dict["VX"])*throttle - sys_param_dict["Cr0"] - sys_param_dict["Cr2"]*(state_action_dict["VX"]**2)
+        Ffy = sys_param_dict["Df"] * torch.sin(sys_param_dict["Cf"] * torch.atan(sys_param_dict["Bf"] * alphaf))
+        Fry = sys_param_dict["Dr"] * torch.sin(sys_param_dict["Cr"] * torch.atan(sys_param_dict["Br"] * alphar))
+        dxdt = torch.zeros(len(x), len(self.state)).to(device)
+        dxdt[:,0] = 1/self.vehicle_specs["mass"] * (Frx - Ffy*torch.sin(steering)) + state_action_dict["VY"]*state_action_dict["YAW_RATE"]
+        dxdt[:,1] = 1/self.vehicle_specs["mass"] * (Fry + Ffy*torch.cos(steering)) - state_action_dict["VX"]*state_action_dict["YAW_RATE"]
+        dxdt[:,2] = 1/sys_param_dict["Izz"] * (Ffy*self.vehicle_specs["lf"]*torch.cos(steering) - Fry*self.vehicle_specs["lr"])
+        dxdt[:,3] = state_action_dict["THROTTLE_CMD"]
+        dxdt[:,4] = state_action_dict["STEERING_CMD"]
+        dxdt[:,:3] *= state_action_dict["delta_t"][:, None]
+        return x[:,-1,:len(self.state)] + dxdt
 
 
-    def forward(self, x, h0=None):
-        for i in range(len(self.feed_forward)):
-            if i == 0:
-                if isinstance(self.feed_forward[i], torch.nn.RNNBase):
-                    ff, h0 = self.feed_forward[0](x[:,:,:7], h0)
-                else:
-                    ff = self.feed_forward[i](torch.reshape(x[:,:,:7], (len(x), -1)))
-            else:
-                if isinstance(self.feed_forward[i], torch.nn.RNNBase):
-                    ff, h0 = self.feed_forward[0](ff, h0)
-                else:
-                    ff = self.feed_forward[i](ff)
-        o = self.differential_equation(x, ff)
-        return o, h0, ff
-        
+class DeepPacejkaModel(ModelBase):
+    def __init__(self, param_dict):
+        super().__init__(param_dict)
 
     def differential_equation(self, x, output):
         sys_param_dict, _ = self.unpack_sys_params(output)
@@ -191,33 +160,6 @@ class DeepPacejkaModel(nn.Module):
         dxdt[:,4] = state_action_dict["STEERING_CMD"]
         dxdt[:,:3] *= state_action_dict["delta_t"][:, None]
         return x[:,-1,:len(self.state)] + dxdt
-
-
-    def unpack_sys_params(self, o):
-        sys_params_dict = dict()
-        for i in range(len(self.sys_params)):
-            sys_params_dict[self.sys_params[i]] = o[:,i]
-        ground_truth_dict =  dict()
-        for p in self.param_dict["PARAMETERS"]:
-            ground_truth_dict.update(p)
-        return sys_params_dict, ground_truth_dict
-
-    def unpack_state_actions(self, x):
-        state_action_dict = dict()
-        global_index = 0
-        for i in range(len(self.state)):
-            state_action_dict[self.state[i]] = x[:,-1, global_index]
-            global_index += 1
-        for i in range(len(self.actions)):
-            state_action_dict[self.actions[i]] = x[:,-1, global_index]
-            global_index += 1
-        state_action_dict["delta_t"] = x[:,-1, global_index] - x[:, -2, global_index]
-        return state_action_dict
-
-    def init_hidden(self, batch_size):
-        weight = next(self.parameters()).data
-        hidden = weight.new(self.rnn_n_layers, batch_size, self.rnn_hiden_dim).zero_().to(device)
-        return hidden
 
 
 string_to_model = {
