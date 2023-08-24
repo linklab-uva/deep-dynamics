75abc56b2d4fc6f5f24e0636ee79755f6ebda205
john-chrosniak <chrosniakj@gmail.com>
2023-08-24 15:37:55 -0400

    added average and final displacement error metrics to iac data

--- deep_dynamics/visualize/plot_predictions_iac.py
+++ deep_dynamics/visualize/plot_predictions_iac.py
@@ -5,6 +5,7 @@ import matplotlib
 import csv
 import yaml
 import torch
+from tqdm import tqdm
 from deep_dynamics.model.models import DeepDynamicsDataset, string_to_model
 from deep_dynamics.tools.csv_parser import write_dataset
 
@@ -46,38 +47,45 @@ ddm.eval()
 ddm.load_state_dict(torch.load(state_dict))
 features, labels, poses = write_dataset(dataset_file, ddm.horizon, save=False)
 samples = set(range(2500, len(features), 500))
-ddm_predictions = np.zeros((len(samples), 6, HORIZON))
 ddm_dataset = DeepDynamicsDataset(features, labels)
+ddm_predictions = np.zeros((len(ddm_dataset)-HORIZON, 6, HORIZON+1))
 ddm_data_loader = torch.utils.data.DataLoader(ddm_dataset, batch_size=1, shuffle=False)
-global_idx = 0
 idt = 0
-for inputs, labels, norm_inputs in ddm_data_loader:
-	if global_idx in samples:
-		if ddm.is_rnn:
-			h = ddm.init_hidden(inputs.shape[0])
-			h = h.data
-		inputs, labels, norm_inputs = inputs.to(device), labels.to(device), norm_inputs.to(device)
-		if ddm.is_rnn:
-			_, h, ddm_output, _ = ddm(inputs, norm_inputs, h)
-		else:
-			_, _, ddm_output, _ = ddm(inputs, norm_inputs)
-		# Simulate model
-		ddm_predictions[idt,:3,0] = poses[global_idx, :3]
-		ddm_predictions[idt,3:,0] = features[global_idx,-1,:3]
-		for idh in range(0, HORIZON-1):
-			ddm_predictions[idt,0,idh+1] = ddm_predictions[idt,0,idh] + (ddm_predictions[idt,3,idh]*np.cos(ddm_predictions[idt,2,idh]) - ddm_predictions[idt,4,idh]*np.sin(ddm_predictions[idt,2,idh])) * Ts
-			ddm_predictions[idt,1,idh+1] = ddm_predictions[idt,1,idh] + (ddm_predictions[idt,3,idh]*np.sin(ddm_predictions[idt,2,idh]) + ddm_predictions[idt,4,idh]*np.cos(ddm_predictions[idt,2,idh])) * Ts
-			ddm_predictions[idt,2,idh+1] = ddm_predictions[idt,2,idh] + ddm_predictions[idt,5,idh] * Ts
-			ddm_input = np.array([*ddm_predictions[idt,3:,idh], *features[global_idx+idh, -1, 3:]]).reshape(1,1,-1)
-			dxdt, _ = ddm.differential_equation(torch.from_numpy(ddm_input).to(device), ddm_output, Ts) 
-			dxdt = dxdt.cpu().detach().numpy()[-1]
-			ddm_predictions[idt,3,idh+1] = dxdt[0]
-			ddm_predictions[idt,4,idh+1] = dxdt[1]
-			ddm_predictions[idt,5,idh+1] = dxdt[2]
-		idt += 1
-	global_idx += 1
-
-	
+average_displacement_error = 0.0
+final_displacement_error = 0.0
+for inputs, labels, norm_inputs in tqdm(ddm_data_loader, total=len(ddm_predictions)):
+	if idt == len(ddm_predictions):
+		break
+	if ddm.is_rnn:
+		h = ddm.init_hidden(inputs.shape[0])
+		h = h.data
+	inputs, labels, norm_inputs = inputs.to(device), labels.to(device), norm_inputs.to(device)
+	if ddm.is_rnn:
+		_, h, ddm_output, _ = ddm(inputs, norm_inputs, h)
+	else:
+		_, _, ddm_output, _ = ddm(inputs, norm_inputs)
+	# Simulate model
+	ddm_predictions[idt,:3,0] = poses[idt, :3]
+	ddm_predictions[idt,3:,0] = features[idt,-1,:3]
+	displacement_error = 0
+	for idh in range(0, HORIZON):
+		ddm_predictions[idt,0,idh+1] = ddm_predictions[idt,0,idh] + (ddm_predictions[idt,3,idh]*np.cos(ddm_predictions[idt,2,idh]) - ddm_predictions[idt,4,idh]*np.sin(ddm_predictions[idt,2,idh])) * Ts
+		ddm_predictions[idt,1,idh+1] = ddm_predictions[idt,1,idh] + (ddm_predictions[idt,3,idh]*np.sin(ddm_predictions[idt,2,idh]) + ddm_predictions[idt,4,idh]*np.cos(ddm_predictions[idt,2,idh])) * Ts
+		ddm_predictions[idt,2,idh+1] = ddm_predictions[idt,2,idh] + ddm_predictions[idt,5,idh] * Ts
+		ddm_input = np.array([*ddm_predictions[idt,3:,idh], *features[idt+idh, -1, 3:]]).reshape(1,1,-1)
+		dxdt, _ = ddm.differential_equation(torch.from_numpy(ddm_input).to(device), ddm_output, Ts) 
+		dxdt = dxdt.cpu().detach().numpy()[-1]
+		ddm_predictions[idt,3,idh+1] = dxdt[0]
+		ddm_predictions[idt,4,idh+1] = dxdt[1]
+		ddm_predictions[idt,5,idh+1] = dxdt[2]
+		displacement_error += np.sum((ddm_predictions[idt,:2,idh+1] - poses[idt+idh,:2])**2)
+	average_displacement_error += displacement_error / HORIZON
+	final_displacement_error += np.sum((ddm_predictions[idt,:2,idh+1] - poses[idt+idh,:2])**2)
+	idt += 1
+average_displacement_error /= len(ddm_predictions)
+final_displacement_error /= len(ddm_predictions)
+print("DDM Average Displacement Error:", average_displacement_error)
+print("DDM Final Displacement Error:", final_displacement_error)
 
 param_file = "../cfgs/model/deep_pacejka_iac.yaml"
 state_dict = "../output/deep_pacejka_iac/manual/epoch_321.pth"
@@ -88,36 +96,46 @@ dpm.cuda()
 dpm.load_state_dict(torch.load(state_dict))
 features, labels, poses = write_dataset(dataset_file, dpm.horizon, save=False)
 samples = list(range(2500, len(features), 500))
-dpm_predictions = np.zeros((len(samples), 6, HORIZON))
 dpm_dataset = DeepDynamicsDataset(features, labels)
+dpm_predictions = np.zeros((len(dpm_dataset)-HORIZON, 6, HORIZON+1))
 dpm_data_loader = torch.utils.data.DataLoader(dpm_dataset, batch_size=1, shuffle=False)
-global_idx = 0
 idt = 0
-for inputs, labels, norm_inputs in dpm_data_loader:
-	if global_idx in samples:
-		if dpm.is_rnn:
-			h = dpm.init_hidden(inputs.shape[0])
-			h = h.data
-		inputs, labels, norm_inputs = inputs.to(device), labels.to(device), norm_inputs.to(device)
-		if dpm.is_rnn:
-			_, h, dpm_output, _ = dpm(inputs, norm_inputs, h)
-		else:
-			_, _, dpm_output, _ = dpm(inputs, norm_inputs)
-		# Simulate model
-		dpm_predictions[idt,:3,0] = poses[global_idx, :3]
-		dpm_predictions[idt,3:,0] = features[global_idx,-1,:3]
-		for idh in range(0, HORIZON-1):
-			dpm_predictions[idt,0,idh+1] = dpm_predictions[idt,0,idh] + (dpm_predictions[idt,3,idh]*np.cos(dpm_predictions[idt,2,idh]) - dpm_predictions[idt,4,idh]*np.sin(dpm_predictions[idt,2,idh])) * Ts
-			dpm_predictions[idt,1,idh+1] = dpm_predictions[idt,1,idh] + (dpm_predictions[idt,3,idh]*np.sin(dpm_predictions[idt,2,idh]) + dpm_predictions[idt,4,idh]*np.cos(dpm_predictions[idt,2,idh])) * Ts
-			dpm_predictions[idt,2,idh+1] = dpm_predictions[idt,2,idh] + dpm_predictions[idt,5,idh] * Ts
-			dpm_input = np.array([*dpm_predictions[idt,3:,idh], *features[global_idx+idh, -1, 3:]]).reshape(1,1,-1)
-			dxdt, _ = dpm.differential_equation(torch.from_numpy(dpm_input).to(device), dpm_output, Ts) 
-			dxdt = dxdt.cpu().detach().numpy()[-1]
-			dpm_predictions[idt,3,idh+1] = dxdt[0]
-			dpm_predictions[idt,4,idh+1] = dxdt[1]
-			dpm_predictions[idt,5,idh+1] = dxdt[2]
-		idt += 1
-	global_idx += 1
+average_displacement_error = 0.0
+final_displacement_error = 0.0
+for inputs, labels, norm_inputs in tqdm(dpm_data_loader, total=len(dpm_predictions)):
+	if idt == len(dpm_predictions):
+		break
+	if dpm.is_rnn:
+		h = dpm.init_hidden(inputs.shape[0])
+		h = h.data
+	inputs, labels, norm_inputs = inputs.to(device), labels.to(device), norm_inputs.to(device)
+	if dpm.is_rnn:
+		_, h, dpm_output, _ = dpm(inputs, norm_inputs, h)
+	else:
+		_, _, dpm_output, _ = dpm(inputs, norm_inputs)
+	# Simulate model
+	dpm_predictions[idt,:3,0] = poses[idt, :3]
+	dpm_predictions[idt,3:,0] = features[idt,-1,:3]
+	displacement_error = 0
+	for idh in range(0, HORIZON):
+		dpm_predictions[idt,0,idh+1] = dpm_predictions[idt,0,idh] + (dpm_predictions[idt,3,idh]*np.cos(dpm_predictions[idt,2,idh]) - dpm_predictions[idt,4,idh]*np.sin(dpm_predictions[idt,2,idh])) * Ts
+		dpm_predictions[idt,1,idh+1] = dpm_predictions[idt,1,idh] + (dpm_predictions[idt,3,idh]*np.sin(dpm_predictions[idt,2,idh]) + dpm_predictions[idt,4,idh]*np.cos(dpm_predictions[idt,2,idh])) * Ts
+		dpm_predictions[idt,2,idh+1] = dpm_predictions[idt,2,idh] + dpm_predictions[idt,5,idh] * Ts
+		dpm_input = np.array([*dpm_predictions[idt,3:,idh], *features[idt+idh, -1, 3:]]).reshape(1,1,-1)
+		dxdt, _ = dpm.differential_equation(torch.from_numpy(dpm_input).to(device), dpm_output, Ts) 
+		dxdt = dxdt.cpu().detach().numpy()[-1]
+		dpm_predictions[idt,3,idh+1] = dxdt[0]
+		dpm_predictions[idt,4,idh+1] = dxdt[1]
+		dpm_predictions[idt,5,idh+1] = dxdt[2]
+		displacement_error += np.sum((dpm_predictions[idt,:2,idh+1] - poses[idt+idh,:2])**2)
+	average_displacement_error += displacement_error / HORIZON
+	final_displacement_error += np.sum((dpm_predictions[idt,:2,idh+1] - poses[idt+idh,:2])**2)
+	idt += 1
+average_displacement_error /= len(dpm_predictions)
+final_displacement_error /= len(dpm_predictions)
+print("DPM Average Displacement Error:", average_displacement_error)
+print("DDM Final Displacement Error:", final_displacement_error)
+
 
 #####################################################################
 # plots
@@ -125,12 +143,12 @@ font = {'family' : 'normal',
         'weight' : 'normal',
         'size'   : 22}
 matplotlib.rc('font', **font)
-for idx in range(len(samples)):
+for idx in samples:
 	plt.figure(figsize=(12,8))
 	plt.axis('equal')
 	plt.plot(inner_bounds[:,0], inner_bounds[:,1],'k', lw=0.5, alpha=0.5)
 	plt.plot(outer_bounds[:,0], outer_bounds[:,1],'k', lw=0.5, alpha=0.5)
-	plt.plot(poses[samples[idx]-500:samples[idx]+500,0], poses[samples[idx]-500:samples[idx]+500:,1], 'b', lw=1, label='Ground Truth')
+	plt.plot(poses[idx-500:idx+500,0], poses[idx-500:idx+500:,1], 'b', lw=1, label='Ground Truth')
 	plt.xlabel('$x$ [m]')
 	plt.ylabel('$y$ [m]')
 	plt.plot(ddm_predictions[idx, 0, :], ddm_predictions[idx, 1, :], '--go', label="Deep Dynamics")
