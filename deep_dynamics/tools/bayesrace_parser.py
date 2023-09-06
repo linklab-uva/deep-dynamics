75a4c2ef1b494c45ee56239beee851177a962b24
john-chrosniak <chrosniakj@gmail.com>
2023-09-05 21:17:09 -0400

    added in code that was somehow lost in the void

--- deep_dynamics/tools/bayesrace_parser.py
+++ deep_dynamics/tools/bayesrace_parser.py
@@ -10,12 +10,13 @@ def write_dataset(input_file, horizon, save=True):
     inputs = data["inputs"]
     poses = data["states"][:6,:].T
     odometry = states[3:,:].T # vx, vy, yaw_rate, throttle_fb, steering_fb
-    throttle_cmd = inputs[0,:]
-    steering_cmd = inputs[1,:]
+    throttle_cmd = inputs[0,:] - [0.0, *inputs[0,:-1]]
+    steering_cmd = inputs[1,:] - [0.0, *inputs[1,:-1]]
     features = np.zeros((len(odometry) - horizon - 1,  horizon, 7), dtype=np.double)
     labels = np.zeros((len(odometry) - horizon - 1, 3), dtype=np.double)
     for i in tqdm(range(len(throttle_cmd) - horizon), desc="Compiling dataset"):
         features[i] = np.array([*odometry[i:i+horizon].T, throttle_cmd[i:i+horizon], steering_cmd[i:i+horizon]]).T
+        labels[i] = np.array([*odometry[i+horizon]])[:3]
     print("Final features shape:", features.shape)
     print("Final labels shape:", labels.shape)
     if save:
